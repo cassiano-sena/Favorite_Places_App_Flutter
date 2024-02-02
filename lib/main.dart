f069ed4d24a587beeedcfd811628d4fd5fcd0518
Cassiano <csdscr@gmail.com>
2024-02-02 16:29:10 -0300

    02-01-24 16:28

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:favorite_places_app/screens/places.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:favorite_places_app/screens/places.dart';
 
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
@@ -28,7 +28,7 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-    const MyApp(),
+    const ProviderScope(child: MyApp()),
   );
 }
 
@@ -40,7 +40,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Great Places',
       theme: theme,
-      // home: ...,
       home: const PlacesScreen(),
     );
   }
